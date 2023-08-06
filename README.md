aa7b768887ce81841bd919ab9b4b24e0f66c1880
Chai Phonbopit <phonbopit@gmail.com>
2023-08-06 17:30:40 +0700

    add graceful shutdown

---
+++ README.md
@@ -0,0 +1,6 @@
+## Hello Axum
+
+References
+
+- https://github.com/tokio-rs/axum
+- https://docs.rs/crate/axum/latest
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 use axum::{routing::get, Router};
+use tokio::signal;
+
 use std::net::SocketAddr;
 
 #[tokio::main]
@@ -6,12 +8,42 @@ async fn main() {
     let app = Router::new().route("/", get(root));
     let addr = SocketAddr::from(([127, 0, 0, 1], 9900));
 
+    println!("listening on {}", addr);
+
     axum::Server::bind(&addr)
         .serve(app.into_make_service())
+        .with_graceful_shutdown(shutdown_signal())
         .await
         .unwrap();
 }
 
 async fn root() -> &'static str {
     "Hello Axum!!"
+}
+
+async fn shutdown_signal() {
+    let ctrl_c = async {
+        signal::ctrl_c()
+            .await
+            .expect("failed to install Ctrl+C handler");
+    };
+
+    #[cfg(unix)]
+    let terminate = async {
+        signal::unix::signal(signal::unix::SignalKind::terminate())
+            .expect("failed to install SIGTERM handler")
+            .recv()
+            .await
+            .expect("failed to receive SIGTERM")
+    };
+
+    #[cfg(not(unix))]
+    let terminate = std::future::pending();
+
+    tokio::select! {
+        _ = ctrl_c => {},
+        _ = terminate => {},
+    }
+
+    println!("signal received, starting graceful shutdown");
 }
